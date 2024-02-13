9112db53039b263f8d6e0af2191bc97c4833b67a
dokyu <dokyung36@naver.com>
2024-02-13 15:16:10 +0900

    test file plus

---
+++ test.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def testUploadFile():
+    files = {"file": ("testImage.jpg", open("test_image.jpg", "rb"))}
+    response = client.post("/AICOSS/image/prediction", files=files)
+
+    assert response.status_code == 200
+    assert response.json() != None
