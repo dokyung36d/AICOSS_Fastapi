4cd53f36f34c439274ac4573aa8c5313a478a6e3
dokyu <dokyung36@naver.com>
2025-05-01 22:48:18 +0900

    changed to download pth file from aws s3

--- aiModel.py
+++ aiModel.py
@@ -2,6 +2,8 @@ from timm.models import create_model
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
+from key import SECRET_KEY, ACCESS_KEY, REGION, WEIGHT_BUCKET, WEIGHT_KEY
+import boto3
 
 model = create_model(
     'DaViT_base',
@@ -11,6 +13,15 @@ model = create_model(
 
 model = model.to("cpu")
 
+s3_client = boto3.client(
+    's3',
+    region_name=REGION,
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+)
+
+
 checkpoint_path = '2023_AICOSS_model_weight_EggTheProtein.pth'
+s3_client.download_file(WEIGHT_BUCKET, WEIGHT_KEY, checkpoint_path)
 checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
 model.load_state_dict(checkpoint)
